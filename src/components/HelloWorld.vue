588db20c1012dd8d0e7f50997a9fd8a078cc9309
Levan Begashvili <life.long.learner127@outlook.com>
2020-07-23 23:00:19 +0400

    loader; load solider glb

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div id="container"></div>
 </template>
 
 <script>
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      renderer: null,
+      scene: null,
+      camera: null
+    }
+  },
+  mounted () {
+    let container = document.getElementById( 'container' );
+
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+    this.camera.position.set(1, 2, -3)
+    this.camera.lookAt(0, 1, 0)
+
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0xa0a0a0)
+    this.scene.fog = new THREE.Fog(0xa0a0a0, 10, 50)
+
+    let hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444)
+    hemiLight.position.set(0, 20, 0)
+    this.scene.add(hemiLight)
+
+    let dirLight = new THREE.DirectionalLight(0xffffff)
+    dirLight.position.set(-3, 10, -10)
+    dirLight.castShadow = true
+    dirLight.shadow.camera.top = 2
+    dirLight.shadow.camera.bottom = - 2
+    dirLight.shadow.camera.left = - 2
+    dirLight.shadow.camera.right = 2
+    dirLight.shadow.camera.near = 0.1
+    dirLight.shadow.camera.far = 40
+    this.scene.add(dirLight)
+
+    var mesh = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry( 100, 100 ),
+      new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } )
+    )
+    mesh.rotation.x = - Math.PI / 2
+    mesh.receiveShadow = true
+    this.scene.add( mesh )
+
+    let loader = new GLTFLoader()
+    let vm = this
+		loader.load('Soldier.glb', function (gltf) {
+      let model = gltf.scene
+			vm.scene.add(model)
+      model.traverse( function ( object ) {
+        if ( object.isMesh ) {
+          object.castShadow = true
+        }
+      })
+      vm.animate()
+    })
+
+    this.renderer = new THREE.WebGLRenderer( { antialias: true } )
+    this.renderer.setPixelRatio( window.devicePixelRatio )
+    this.renderer.setSize( window.innerWidth, window.innerHeight )
+    this.renderer.outputEncoding = THREE.sRGBEncoding
+    this.renderer.shadowMap.enabled = true
+    container.appendChild( this.renderer.domElement )
+  },
+  methods: {
+    animate() {
+      requestAnimationFrame(this.animate)
+      this.renderer.render( this.scene, this.camera )
+    }
   }
 }
 </script>
